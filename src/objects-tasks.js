0f10bbe71408bb0dd55735878d62aca4b2ba8c1f
dn071099umi <mariia.uzlova@privatbank.ua>
2024-02-21 19:09:09 +0200

    feat: add 1 tasks (function isEmptyObject)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -97,8 +97,9 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  const keys = Object.keys(obj);
+  return keys.length === 0;
 }
 
 /**
