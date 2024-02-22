3186c213b15fbce5ec90a516b3749125df2ed154
dn071099umi <mariia.uzlova@privatbank.ua>
2024-02-22 13:37:14 +0200

    feat: add task function sortCitiesArray

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -252,8 +252,14 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  arr.sort((a, b) => {
+    if (a.country === b.country) {
+      return -(a.city < b.city);
+    }
+    return -(a.country < b.country);
+  });
+  return arr;
 }
 
 /**
